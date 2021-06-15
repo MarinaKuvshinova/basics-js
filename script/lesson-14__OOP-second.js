804c440b2115e23ffa5ca7e6bb60e15c339f17c1
MarinaKuvshinova <pn-05@ukr.net>
2021-06-15 23:05:39 +0300

    additionally for lesson-14

--- script/lesson-14__OOP-second.js
+++ script/lesson-14__OOP-second.js
@@ -9,7 +9,6 @@ const DomElement = function (selector, height, width, bg, fontSize) {
 };
 
 DomElement.prototype.createElement = function () {
-    console.log(this);
 
     let firstChar = this.selector.slice(0, 1);
     let elem;
@@ -31,14 +30,61 @@ DomElement.prototype.createElement = function () {
         font-size: ${this.fontSize}px;`;
     
     elem.textContent = 'Hello workd!';
+   
 
-    document.querySelector('.wrapper').append(elem);
+    document.querySelector('body').append(elem);
 
 };
 
 
-let elem1 = new DomElement('#parag', 20, 30, 'red', 10);
-elem1.createElement();
 
-let elem2 = new DomElement('.block', 200, 100, 'green', 20);
-elem2.createElement();
+// let elem1 = new DomElement('#parag', 20, 30, 'red', 10);
+// elem1.createElement();
+
+// let elem2 = new DomElement('.block', 200, 100, 'green', 20);
+// elem2.createElement();
+
+
+document.addEventListener("DOMContentLoaded", (event) => {
+
+    let elem = new DomElement('.absolute', 100, 100, 'green', 20);
+    elem.createElement();
+
+});
+
+document.addEventListener('keydown', (e) => {
+    let block = document.querySelector('.absolute'),
+        rect = block.getBoundingClientRect();
+
+    if (e.code === 'ArrowUp') {
+
+        if (rect.top - 10 <= 0) {
+            block.style.top = 0;
+        } else {
+            block.style.top = rect.top - 10 + 'px';
+        }
+    }
+
+    if (e.code === 'ArrowDown') {
+        if (rect.top + 10 + rect.height >= window.innerHeight) {
+            block.style.top = window.innerHeight;
+        } else {
+            block.style.top = rect.top + 10 + 'px';
+        }
+    }
+    if (e.code === 'ArrowLeft') {
+        if (rect.left - 10 <= 0) {
+            block.style.left = 0;
+        } else {
+            block.style.left = rect.left - 10 + 'px';
+        }
+    }
+
+    if (e.code === 'ArrowRight') {
+        if (rect.left + 10 + rect.width >= window.innerWidth) {
+            block.style.left = window.innerWidth;
+        } else {
+            block.style.left = rect.left + 10 + 'px';
+        }
+    }
+});
